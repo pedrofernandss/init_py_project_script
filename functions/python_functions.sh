adcd8559913efb569831b72c2f4139e7ac03469b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-08 16:51:11 -0300

    create function to copy py template

--- functions/python_functions.sh
+++ functions/python_functions.sh
@@ -13,3 +13,12 @@ install_pyton() {
     sudo apt-get install -y python
     echo "The latest python was successfully installed"
 }
+
+create_py_structure_from_template() {
+    # Clona o repositório
+    git clone https://github.com/pedrofernandss/template.git
+    # Move os arquivos pra pasta mae pra ficar mais organizado
+    mv -f template/* ./ 2>/dev/null
+    #Exclui a pasta do repositório remoto de template
+    rm -rf template
+}
