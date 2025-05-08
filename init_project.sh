5d0d2f491eff1ce71b456823124daad91ae6fd7d
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-08 17:38:15 -0300

    finishing project initiation

--- init_project.sh
+++ init_project.sh
@@ -2,17 +2,20 @@
 source ./functions/git_functions.sh
 source ./functions/python_functions.sh
 
-echo "Which will be the name of your python project?"
-echo "On the name, please do not use spaces! Instead, use _ (underline) or - (hífen)"
+echo "------------------------------------------------------"
+echo "### Which will be the name of your python project?"
+echo "On the name, please do not use spaces! Instead, use _ (underline) or - (hifen)"
+echo "------------------------------------------------------"
 read -r project_name
 
-echo "Ok, let's create your project repository"
+echo "### Ok, let's create your project repository"
 
 if ! get_git; then
     install_git
-    echo "What are your name and your most usual email?"
+    echo "### What are your name and your most usual email?"
     read -r name email
     configure_git "$name" "$email"
+fi
 
 mkdir "$project_name"
 cd "$project_name"
@@ -21,5 +24,9 @@ git init
 
 if ! get_python; then
     install_python
+fi
 
-create_py_structure_from_template
+create_py_api_struct_from_template
+install_dependencies
+
+echo "### To run the project use: uvicorn app.main:app --reload"
